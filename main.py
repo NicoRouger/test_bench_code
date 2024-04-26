38feb937cb6db49753ad166a986709e7f0e0249b
Luiz Villa <lflavado@laas.fr>
2024-04-26 14:18:53 +0200

    updated the main to allow python control of the spin

--- main.py
+++ main.py
@@ -5,23 +5,65 @@ import time
 
 from lib.KEYTHLEY2000 import KEYTHLEY2000
 
-# from gui import gui
+from communication_protocol_library.src.Twist_Class import Twist_Device
+from communication_protocol_library.src import find_devices
 
 import sys
 import threading
 
+import serial
+
+
+
 def main_app(shared_data):
-	rm = pyvisa.ResourceManager('@py')
-	d = KEYTHLEY2000(rm, 'ASRL/dev/cu.usbserial-2110::INSTR')
-	d.check(role="temp1", name='KEITHLEY INSTRUMENTS INC.,MODEL 2000,1308393,A20  /A02', port="A")
+	# rm = pyvisa.ResourceManager('@py')
+	# d = KEYTHLEY2000(rm, 'ASRL/dev/cu.usbserial-2110::INSTR')
+	# d.check(role="temp1", name='KEITHLEY INSTRUMENTS INC.,MODEL 2000,1308393,A20  /A02', port="A")
+
+	twist_vid = 0x2fe3
+	twist_pid = 0x0100
+
+	Twist_ports = find_devices.find_twist_device_ports(twist_vid, twist_pid)
+	print(Twist_ports)
+
+	Twist = Twist_Device(twist_port= Twist_ports[0])
+
+
+	message = Twist.sendCommand("IDLE")
+	print(message)
+
+	message = Twist.sendCommand( "BUCK", "LEG2", "ON")
+	print(message)
+	message = Twist.sendCommand( "BUCK", "LEG1", "ON")
+	print(message)
+
+	message = Twist.sendCommand("LEG","LEG1","ON")
+	print(message)
+	message = Twist.sendCommand("LEG","LEG2","ON")
+	print(message)
+
+	message = Twist.sendCommand("DEAD_TIME_RISING", "LEG1", 300)
+	print(message)
+	message = Twist.sendCommand("DEAD_TIME_FALLING", "LEG1", 100)
+	print(message)
+
+	message = Twist.sendCommand("POWER_ON")
+	print(message)
+
+	message = Twist.sendCommand("DUTY", "LEG1", 0.5)
+	print(message)
+	message = Twist.sendCommand("DUTY", "LEG2", 0.5)
+	print(message)
+
+	message = Twist.sendCommand("PHASE_SHIFT", "LEG2", 180)
 
 	# d.displayText("OwnTech")
 	# d.animateText("OwnTech")
 
-	d.configTemp(rjunc=26)
-	d.getTemp()
-	print(d.measureVoltage())
-	print(d.measureCurrent())
+	# d.configTemp(rjunc=26)
+	# d.getTemp()
+	# print(d.measureVoltage())
+	# print(d.measureCurrent())
 
 if __name__ == "__main__":
 	shared_data = Queue()
